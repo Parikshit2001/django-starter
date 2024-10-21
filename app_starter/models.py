a8fc27f5909c2ccf59e3941ce86fc4a50ff8ab78
Parikshit <parikshitgupta2001@gmail.com>
2024-10-21 16:54:09 +0530

    Add relationsip in db, OneToOne, OneToMany and ManyToMany, also customize admin page

--- app_starter/models.py
+++ app_starter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -20,3 +21,42 @@ class ChaiVariety(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# One to many
+class ChaiReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chai = models.ForeignKey(
+        ChaiVariety, on_delete=models.CASCADE, related_name="reviews"
+    )
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.chai.name}"
+
+
+# Many to many
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    chai_varieties = models.ManyToManyField(ChaiVariety, related_name="stores")
+
+    def __str__(self):
+        return self.name
+
+
+# One to one
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(
+        ChaiVariety,
+        on_delete=models.CASCADE,
+        related_name="certificate",
+    )
+    certificate_number = models.CharField(max_length=50)
+    issue_date = models.DateField(auto_now_add=True)
+    expiration_date = models.DateField()
+
+    def __str__(self):
+        return f"Certificate for {self.chai.name}"
